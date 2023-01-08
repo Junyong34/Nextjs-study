bee2b8e02c2b0b44cf5da5896a79c2a387b62ef2
JunYong <wnsdyd21@gmail.com>
2023-01-08 23:15:49 +0900

    init: firebase 초기 셋팅 및 test api 생성

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Nextjs 알아보기
+
+
+.env 파일 필수. firebase 접속정보
+```
+publicApiKey=퍼블릭key
+projectId=프로젝트id
+FIREBASE_AUTH_HOST={프로젝트id}.firebaseapp.com
+privateKey=프라이빗key
+clientEmail=이메일
+PORT=3000
+HOST=localhost
+PROTOCOL=http
+```
---
+++ models/firebase_admin.ts
@@ -0,0 +1,60 @@
+import * as admin from 'firebase-admin';
+
+interface Config {
+  credential: {
+    privateKey: string;
+    clientEmail: string;
+    projectId: string;
+  };
+}
+
+export default class FirebaseAdmin {
+  public static instance: FirebaseAdmin;
+
+  private init = false;
+
+  public static getInstance(): FirebaseAdmin {
+    if (FirebaseAdmin.instance === undefined || FirebaseAdmin.instance === null) {
+      // 초기화 진행
+      FirebaseAdmin.instance = new FirebaseAdmin();
+      // 환경 초기화
+      FirebaseAdmin.instance.bootstrap();
+    }
+
+    return FirebaseAdmin.instance;
+  }
+
+  private bootstrap(): void {
+    const haveApp = admin.apps.length !== 0;
+    if (haveApp) {
+      this.init = true;
+      return;
+    }
+
+    const config: Config = {
+      credential: {
+        projectId: process.env.projectId || '',
+        clientEmail: process.env.clientEmail || '',
+        // 개항문자를 직접 넣어줘야해서 replace 함
+        privateKey: (process.env.privateKey || '').replace(/\\n/gm, '\n'),
+      },
+    };
+    admin.initializeApp({ credential: admin.credential.cert(config.credential) });
+    console.info('bootstrap firebase admin !!');
+  }
+
+  /** firestore를 반환 */
+  public get Firebase(): FirebaseFirestore.Firestore {
+    if (this.init === false) {
+      this.bootstrap();
+    }
+    return admin.firestore();
+  }
+
+  public get Auth(): admin.auth.Auth {
+    if (this.init === false) {
+      this.bootstrap();
+    }
+    return admin.auth();
+  }
+}
